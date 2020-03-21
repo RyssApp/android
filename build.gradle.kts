ee376280ceb62b1e59bdaab543225b0f6e53fa87
Michael Rittmeister <michael@rittmeister.in>
2020-03-21 22:24:46 +0100

    Migrate to kotlin dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,17 +17,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.5.3'
-        
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        classpath("com.android.tools.build:gradle:3.5.3")
     }
 }
 
@@ -38,6 +33,8 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks {
+    task<Delete>("clean") {
+        delete(rootProject.buildDir)
+    }
 }
